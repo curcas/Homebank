a7812f1f336b39f25ebb96b08b5446c3f5b0132f
Curdin Caspar <curdin.caspar@gmail.com>
2018-10-11 22:30:12 +0200

    Add bootstrap

--- src/Homebank.Web/src/index.js
+++ src/Homebank.Web/src/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
 import './index.html';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'bootstrap'
+
 import { Elm } from './elm/Main.elm';
 
 Elm.Main.init({
--- src/Homebank.Web/webpack.config.js
+++ src/Homebank.Web/webpack.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-  mode: "production",
+    mode: "production",
 
-  module: {
-    rules: [{
-      test: /\.html$/,
-      exclude: /node_modules/,
-      loader: 'file-loader?name=[name].[ext]'
-    },
-    {
-      test: /\.elm$/,
-      exclude: [/elm-stuff/, /node_modules/],
-      loader: "elm-webpack-loader",
-      options: {
-        optimize: true
-      }
+    module: {
+        rules: [{
+            test: /\.html$/,
+            exclude: /node_modules/,
+            loader: 'file-loader?name=[name].[ext]'
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.elm$/,
+            exclude: [/elm-stuff/, /node_modules/],
+            loader: "elm-webpack-loader",
+            options: {
+                optimize: true
+            }
+        }]
     }
-    ]
-  }
 };
